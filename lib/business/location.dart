a9aea347eae8b6693668890276a8d842fbc64fe2
Benjamin Fisk <bfisk1711@gmail.com>
2024-02-20 21:49:17 -0600

    Copy Code to new Repo

---
+++ lib/business/location.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+//import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:location_permissions/location_permissions.dart';
+
+class LocationService {
+  static final LocationService _instance = LocationService._internal();
+  final Geolocator geolocator = Geolocator();
+  //Geoflutterfire geo = Geoflutterfire();
+  //GeolocationStatus geolocationStatus;
+  Position position = Position(
+      longitude: 0,
+      latitude: 0,
+      timestamp: DateTime(1, 1, 2024),
+      accuracy: 0.0,
+      altitude: 0.0,
+      altitudeAccuracy: 0.0,
+      heading: 0.0,
+      headingAccuracy: 0.0,
+      speed: 0.0,
+      speedAccuracy: 0.0);
+  bool initizalized = false;
+  LocationSettings locationOptions = const LocationSettings(
+      accuracy: LocationAccuracy.high, distanceFilter: 10);
+  Stream<Position> positionStream = const Stream.empty();
+  late StreamSubscription<Position> positionSub;
+
+  factory LocationService() {
+    return _instance;
+  }
+
+  LocationService._internal() {
+    if (kDebugMode) {
+      print("LocationService created");
+    }
+  }
+
+  Future<bool> setupService({bool reinit = false}) async {
+    try {
+      positionSub.cancel();
+      PermissionStatus status =
+          await LocationPermissions().checkPermissionStatus();
+      // Get permission from user
+      if (kDebugMode) {
+        print("location permissions status checked");
+      }
+      while (status != PermissionStatus.granted) {
+        status = await LocationPermissions().requestPermissions();
+      }
+      if (kDebugMode) {
+        print("location permissions have been granted by user");
+      }
+      // Ensure position is not null after setup
+      //print("Latitude4: ${position.latitude}");
+      //print("Longitiude4: ${position.longitude}");
+      if (kDebugMode) {
+        print("position: $position");
+      }
+      position = await Geolocator.getCurrentPosition();
+      if (kDebugMode) {
+        print("position2: $position");
+      }
+
+      // while (position == null) {
+      //   print(
+      //       "current position is null - using geolocator to get current position now.");
+      //   position = await Geolocator.getCurrentPosition();
+      // }
+      // if (kDebugMode) {
+      //   print("Latitude: ${position.latitude}");
+      //   print("Longitiude: ${position.longitude}");
+      // }
+      // Create position stream and subscribe to keep service's position up to date.
+      positionStream =
+          Geolocator.getPositionStream(locationSettings: locationOptions)
+              .asBroadcastStream();
+      positionSub = positionStream.listen((Position position) {
+        this.position = position;
+        if (kDebugMode) {
+          print("Latitude2: ${position.latitude}");
+          print("Longitiude2: ${position.longitude}");
+        }
+      });
+      if (kDebugMode) {
+        print("LocationService initialized");
+      }
+      return true;
+    } catch (e) {
+      if (kDebugMode) {
+        print("Error initializing LocationService $e");
+      }
+      return false;
+    }
+  }
+
+  // GeoFirePoint getCurrentGeoFirePoint() {
+  //   return geo.point(
+  //       latitude: position.latitude, longitude: position.longitude);
+  // }
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:zipapp/firebase_options.dart';
+import 'package:zipapp/ui/screens/main_screen.dart';
+import 'package:zipapp/ui/screens/profile_screen.dart';
+import 'package:zipapp/ui/screens/root_screen.dart';
+import 'package:zipapp/ui/screens/sign_in_screen.dart';
+import 'package:zipapp/ui/screens/sign_up_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+void main() async {
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  // runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = 'pk_test_Cn8XIP0a25tKPaf80s04Lo1m00dQhI8R0u';
+  await dotenv.load(fileName: 'assets/.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  SharedPreferences.getInstance().then((prefs) {
+    runApp(MainApp(prefs: prefs));
+  });
+}
+
+class MainApp extends StatelessWidget {
+  final SharedPreferences prefs;
+
+  const MainApp({super.key, required this.prefs});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zip Gameday',
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+      routes: <String, WidgetBuilder>{
+        '/root': (BuildContext context) => const RootScreen(),
+        '/signin': (BuildContext context) => const SignInScreen(),
+        '/signup': (BuildContext context) => const SignUpScreen(),
+        '/main': (BuildContext context) => const MainScreen(),
+        '/profile': (BuildContext context) => const ProfileScreen(),
+      },
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.grey,
+      ),
+      home: _handleCurrentScreen(),
+    );
+  }
+
+  Widget _handleCurrentScreen() {
+    return const RootScreen();
+  }
+}
